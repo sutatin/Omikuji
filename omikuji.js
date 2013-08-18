4c9f727667d827bfd0779d4b807746e20c782dc1
sutatin <sutatinsutatin@gmail.com>
2013-08-19 00:57:49 +0900

    おみくじにメッセージも対応させました。

--- omikuji.js
+++ omikuji.js
@@ -15,4 +15,21 @@ function run(){
     var omikujiArray = ["大吉","中吉","小吉","凶"];
     var result = Math.floor(Math.random() * omikujiArray.length);
     document.getElementById("result").innerHTML  =omikujiArray[result];
+
+    var comment = "";
+    switch (omikujiArray[result]){
+        case "大吉":
+            document.getElementById("comment").innerHTML = "今日は一日絶好調！！";
+            break;
+        case "中吉":
+            document.getElementById("comment").innerHTML = "普通が一番だよね！";
+            break;
+        case "小吉":
+            document.getElementById("comment").innerHTML = "そつない一日を過ごしてね！";
+            break;
+        case "凶":
+            document.getElementById("comment").innerHTML = "気をつければ大丈夫！！";
+            break;
+
+    }
 }
